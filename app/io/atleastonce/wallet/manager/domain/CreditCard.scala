56ae4109fec56e4cec97e2bc750c404a3ee83d93
Winston Jr <winstonjr@gmail.com>
2017-08-22 08:00:06 -0300

    adicionar validação a classe de cartão de crédito

--- app/io/atleastonce/wallet/manager/domain/CreditCard.scala
+++ app/io/atleastonce/wallet/manager/domain/CreditCard.scala
@@ -11,11 +11,20 @@ case class CreditCard(id: String,
                       removed: Option[Boolean] = Some(false),
                       transactions: List[Transaction] = List.empty) {
   private val uuidReg = "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$".r
+  private val ccReg = "^[0-9]{16}$".r
+  private val cvvReg = "^[0-9]{3}$".r
+  private val date = LocalDateTime.now()
   require(null != id && id.nonEmpty, "id é obrigatório")
   require(uuidReg.findFirstIn(id).isDefined, "id deve ser um UUID")
   require(null != number && number.nonEmpty, "number é obrigatório")
-  require(16.equals(number.length))
-
+  require(ccReg.findFirstIn(number).isDefined, "número do cartão de crédito deve possuir 16 caracteres numéricos")
+  require(null != cvv && cvv.nonEmpty, "cvv é obrigatório")
+  require(ccReg.findFirstIn(number).isDefined, "cvv do cartão de crédito deve possuir 3 caracteres numéricos")
+  require(dueDate > 0 && dueDate < 29, "Vencimento do cartão deve ser entre os dias 1 e 28 do mês")
+  require(expirationDate.isAfter(
+    LocalDateTime.of(date.getYear, date.getMonthValue, date.getDayOfMonth + 1, 0, 0, 0)),
+    "A data de expiração deve ser maior ou igual a amanhã")
+  require(credit > 0F, "o crédito do cartão deve ser maior que 0")
 
   /**
     * Método responsável por calcular o crédito consolidado disponível no cartão
@@ -43,6 +52,11 @@ case class CreditCard(id: String,
     this.copy(transactions = newTransactionList)
   }
 
+  /**
+    * Método responsável por retornar a informação da data de pagamento
+    *
+    * @return Data de pagamento do cartão
+    */
   def getDueDate: LocalDateTime = {
     val now = LocalDateTime.now
 
@@ -53,10 +67,21 @@ case class CreditCard(id: String,
     }
   }
 
+  /**
+    * Método responsável por retornar a informação de data de pagamento em milisegundos
+    * para facilitar comparações em outras classes
+    *
+    * @return
+    */
   def getDueDateMillis: Long = {
     this.getDueDate.toInstant(ZoneOffset.UTC).toEpochMilli
   }
 
+  /**
+    * Método responsável por retornar se o cartão está válido para uso ou não
+    *
+    * @return True caso o cartão esteja válido False caso contrário
+    */
   def isValid: Boolean = {
     this.expirationDate.isAfter(LocalDateTime.now)
   }
